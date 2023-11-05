b0b6eb5f61204949383d59ee8bfeff943c46046d
Raven Nuega <nuega12@gmail.com>
2023-11-05 01:44:51 -0500

    fix: decoupled skills section

--- components/skills.tsx
+++ components/skills.tsx
@@ -108,158 +108,158 @@
                   </Center>
                 </Card> 
             </Flex>
-            <Flex 
-              gap={{ base: 16, lg: 16, md: 16, sm: 14, xs: 10 }}
-              wrap="wrap"
-              p={{ base: 10, lg: 10, md: 10, sm: 10, xs: 0 }}
-              pt={{ base: 10, lg: 10, md: 10, sm: 10, xs: 10 }}
-              pb={{ base: 10, lg: 10, md: 10, sm: 10, xs: 10 }}
-              my={{ base: 0, lg: 0, md: 0, sm: 4, xs: 10 }}
-              alignItems="center"
-              justify="center" 
-            >
-              <Center>
-                <Card marginTop={'5'} >
-                  <CardBody padding={'10'} >
-                    <Heading textAlign={'center'} justifyContent={'center'}>
-                      Tech Stack
-                    </Heading>
-                    <Wrap justify={'center'} p={'10'} spacing={'8'}>
-                      <Image 
-                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"
-                        alt="python logo"
-                        cursor="pointer"
-                        boxSize="75px"
-                        objectFit="cover"
-                      />
-                      <Image 
-                        src="https://companieslogo.com/img/orig/SNOW-35164165.png?t=1634190631" 
-                        alt="snowflake logo"
-                        cursor="pointer"
-                        boxSize="75px"
-                        objectFit="scale-down"
-                      />
-                      <Image 
-                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-plain-wordmark.svg" 
-                        alt="logo"
-                        cursor="pointer"
-                        boxSize="75px"
-                        objectFit="cover"
-                      />
-                      <Image 
-                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pandas/pandas-original-wordmark.svg" 
-                        alt="logo"
-                        cursor="pointer"
-                        boxSize="75px"
-                        objectFit="cover"
-                      />
-                      <Image 
-                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/numpy/numpy-original.svg" 
-                        alt="logo"
-                        cursor="pointer"
-                        boxSize="75px"
-                        objectFit="cover"
-                      />
-                      <Image 
-                        src="https://icon.icepanel.io/Technology/svg/scikit-learn.svg" 
-                        alt="logo"
-                        cursor="pointer"
-                        boxSize="75px"
-                        objectFit="cover"
-                      />
-                      <Image 
-                        src="https://asset.brandfetch.io/id72svQ_Rq/ido9m2u7iI.png?updated=1692638484407" 
-                        alt="logo"
-                        cursor="pointer"
-                        boxSize="75px"
-                        objectFit="scale-down"
-                      />
-                      <Image 
-                        src="https://h2o.ai/company/brand-kit/_jcr_content/root/container/section/par/advancedcolumncontro/columns1/advancedcolumncontro/columns0/image.coreimg.png/1683145585206/h-2-o-logo.png" 
-                        alt="logo"
-                        cursor="pointer"
-                        boxSize="75px"
-                        objectFit="cover"
-                      />
-                      <Image 
-                        src="https://upload.wikimedia.org/wikipedia/commons/f/f3/Apache_Spark_logo.svg" 
-                        alt="logo"
-                        cursor="pointer"
-                        boxSize="75px"
-                        bgColor={'white'}
-                        borderRadius={'full'}
-                        p={'2'}
-                        objectFit="scale-down"
-                      />
-                      <Image
-                        src="https://docs.dask.org/en/stable/_images/dask_icon.svg" 
-                        alt="logo"
-                        cursor="pointer"
-                        boxSize="75px"
-                        objectFit="scale-down"
-                      />
-                      <Image 
-                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pytorch/pytorch-original.svg" 
-                        alt="logo"
-                        cursor="pointer"
-                        boxSize="75px"
-                        objectFit="cover"
-                      />
-                      <Image 
-                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tensorflow/tensorflow-original.svg" 
-                        alt="logo"
-                        cursor="pointer"
-                        boxSize="75px"
-                        objectFit="cover"
-                      />
-                      <Image 
-                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg" 
-                        alt="logo"
-                        cursor="pointer"
-                        boxSize="75px"
-                        objectFit="cover"
-                      />
-                      <Image 
-                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kubernetes/kubernetes-plain.svg" 
-                        alt="logo"
-                        cursor="pointer"
-                        boxSize="75px"
-                        objectFit="cover"
-                      />
-                      <Image 
-                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jenkins/jenkins-original.svg" 
-                        alt="logo"
-                        cursor="pointer"
-                        boxSize="75px"
-                        objectFit="cover"
-                      />
-                      <Image 
-                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg" 
-                        alt="logo"
-                        cursor="pointer"
-                        boxSize="75px"
-                        objectFit="cover"
-                      />
-                      <Image 
-                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jupyter/jupyter-original-wordmark.svg" 
-                        alt="logo"
-                        cursor="pointer"
-                        boxSize="75px"
-                        objectFit="cover"
-                      />
-                      <Image 
-                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pytest/pytest-original-wordmark.svg" 
-                        alt="logo"
-                        cursor="pointer"
-                        boxSize="75px"
-                        objectFit="cover"
-                      />
-                    </Wrap>
-                  </CardBody>
-                </Card> 
-              </Center>
-            </Flex>
           </Slider>
+          <Flex 
+            gap={{ base: 16, lg: 16, md: 16, sm: 14, xs: 10 }}
+            wrap="wrap"
+            p={{ base: 10, lg: 10, md: 10, sm: 10, xs: 0 }}
+            pt={{ base: 10, lg: 10, md: 10, sm: 10, xs: 10 }}
+            pb={{ base: 10, lg: 10, md: 10, sm: 10, xs: 10 }}
+            my={{ base: 0, lg: 0, md: 0, sm: 4, xs: 10 }}
+            alignItems="center"
+            justify="center" 
+          >
+            <Center>
+              <Card marginTop={'10'} >
+                <CardBody padding={'10'} >
+                  <Heading textAlign={'center'} justifyContent={'center'}>
+                    Tech Stack
+                  </Heading>
+                  <Wrap justify={'center'} p={'10'} spacing={'8'}>
+                    <Image 
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"
+                      alt="python logo"
+                      cursor="pointer"
+                      boxSize="100px"
+                      objectFit="cover"
+                    />
+                    <Image 
+                      src="https://companieslogo.com/img/orig/SNOW-35164165.png?t=1634190631" 
+                      alt="snowflake logo"
+                      cursor="pointer"
+                      boxSize="100px"
+                      objectFit="scale-down"
+                    />
+                    <Image 
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-plain-wordmark.svg" 
+                      alt="logo"
+                      cursor="pointer"
+                      boxSize="100px"
+                      objectFit="cover"
+                    />
+                    <Image 
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pandas/pandas-original-wordmark.svg" 
+                      alt="logo"
+                      cursor="pointer"
+                      boxSize="100px"
+                      objectFit="cover"
+                    />
+                    <Image 
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/numpy/numpy-original.svg" 
+                      alt="logo"
+                      cursor="pointer"
+                      boxSize="100px"
+                      objectFit="cover"
+                    />
+                    <Image 
+                      src="https://icon.icepanel.io/Technology/svg/scikit-learn.svg" 
+                      alt="logo"
+                      cursor="pointer"
+                      boxSize="100px"
+                      objectFit="cover"
+                    />
+                    <Image 
+                      src="https://asset.brandfetch.io/id72svQ_Rq/ido9m2u7iI.png?updated=1692638484407" 
+                      alt="logo"
+                      cursor="pointer"
+                      boxSize="100px"
+                      objectFit="scale-down"
+                    />
+                    <Image 
+                      src="https://h2o.ai/company/brand-kit/_jcr_content/root/container/section/par/advancedcolumncontro/columns1/advancedcolumncontro/columns0/image.coreimg.png/1683145585206/h-2-o-logo.png" 
+                      alt="logo"
+                      cursor="pointer"
+                      boxSize="100px"
+                      objectFit="cover"
+                    />
+                    <Image 
+                      src="https://upload.wikimedia.org/wikipedia/commons/f/f3/Apache_Spark_logo.svg" 
+                      alt="logo"
+                      cursor="pointer"
+                      boxSize="100px"
+                      bgColor={'white'}
+                      borderRadius={'full'}
+                      p={'2'}
+                      objectFit="scale-down"
+                    />
+                    <Image
+                      src="https://docs.dask.org/en/stable/_images/dask_icon.svg" 
+                      alt="logo"
+                      cursor="pointer"
+                      boxSize="100px"
+                      objectFit="scale-down"
+                    />
+                    <Image 
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pytorch/pytorch-original.svg" 
+                      alt="logo"
+                      cursor="pointer"
+                      boxSize="100px"
+                      objectFit="cover"
+                    />
+                    <Image 
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tensorflow/tensorflow-original.svg" 
+                      alt="logo"
+                      cursor="pointer"
+                      boxSize="100px"
+                      objectFit="cover"
+                    />
+                    <Image 
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg" 
+                      alt="logo"
+                      cursor="pointer"
+                      boxSize="100px"
+                      objectFit="cover"
+                    />
+                    <Image 
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kubernetes/kubernetes-plain.svg" 
+                      alt="logo"
+                      cursor="pointer"
+                      boxSize="100px"
+                      objectFit="cover"
+                    />
+                    <Image 
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jenkins/jenkins-original.svg" 
+                      alt="logo"
+                      cursor="pointer"
+                      boxSize="100px"
+                      objectFit="cover"
+                    />
+                    <Image 
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg" 
+                      alt="logo"
+                      cursor="pointer"
+                      boxSize="100px"
+                      objectFit="cover"
+                    />
+                    <Image 
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jupyter/jupyter-original-wordmark.svg" 
+                      alt="logo"
+                      cursor="pointer"
+                      boxSize="100px"
+                      objectFit="cover"
+                    />
+                    <Image 
+                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pytest/pytest-original-wordmark.svg" 
+                      alt="logo"
+                      cursor="pointer"
+                      boxSize="100px"
+                      objectFit="cover"
+                    />
+                  </Wrap>
+                </CardBody>
+              </Card> 
+            </Center>
+          </Flex>
         </Box>
 			</Stack>
 		)
